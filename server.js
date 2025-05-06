f98592cd5c07a7674291cba80770e607c1500c45
Le Trong Hiep <letronghiep213@gmail.com>
2025-05-07 00:25:25 +0700

    config connection string mongo

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const http = require("http");
 const { initSocket } = require("./src/db/init.socket");
 const app = require("./src/app");
 const PORT = process.env.PORT || 8080;
-// const server = http.createServer(app);
+const server = http.createServer(app);
 
-// server.listen(portSocket);
-// initSocket(server);
-app.listen(PORT, () => {
+initSocket(server);
+server.listen(PORT, () => {
   console.log(`Server starting with ${PORT}`);
 });
